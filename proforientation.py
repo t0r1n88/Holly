e69b0465f39c451e8e03ac5309cf42e0dff08455
Oleg Budaev <t0r1n88@mail.ru>
2022-01-21 13:49:23 +0800

    Реализовано создание базового отчета по профориентации

--- proforientation.py
+++ proforientation.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import openpyxl
 import math
-
+pd.set_option('display.max_columns', None)
 def check_digit(cell):
     """
     Функция для подсчета данных
@@ -20,13 +20,16 @@ def check_digit(cell):
     else:
         return 0
 
-# Создаем список порядковых номеров столбцов которые нужно удалить
-drop_col = [i for i in range(8,153,3)]
 
+# Создаем список порядковых номеров столбцов которые нужно удалить
+drop_col = [i for i in range(8,159,3)]
 # Получаем сырой датафрейм
 temp_df = pd.read_excel('data/Календарь профориентационных мероприятий 2022.xlsx',skiprows=1)
+
 # Удаляем столбцы с ссылками
 df = temp_df.drop(temp_df.columns[drop_col],axis=1)
+df.to_excel('check_col.xlsx',index=False)
+
 
 # Очищаем данные в колонках наименование и место проведения
 df['Наименование ПОО'] = df['Наименование ПОО'].apply(lambda x: x.strip() if str(x) !='nan' else x)
@@ -44,11 +47,9 @@ data_dct = {poo:{} for poo in lst_poo}
 for row in df.itertuples():
     if str(row[1]) and str(row[2]) != 'nan':
         data_dct[row[1]][row[2]] = 0
-print(data_dct)
 length = df.shape[1]
 # Проводим подсчет по местам проведения проб
 for row in df.itertuples():
-    print(row)
     if str(row[2]) == 'nan':
         continue
     for i in range(7,length,2):
@@ -63,9 +64,21 @@ for poo,places in data_dct.items():
     total = 0
     for place,value in places.items():
         total += value
-    data_dct[poo]['Итого'] = total
+    data_dct[poo][f'Итого {poo}'] = total
 
 print(data_dct)
 
-df.to_excel('temp.xlsx',index=False)
+# Обрабатываем словарь для перевода в читаемую таблицу
+first_step_df = pd.DataFrame.from_dict(data_dct,orient='index')
+first_step_df.to_excel('temp.xlsx')
+# развертываем колонки в строки(индексы)
+stack_df = first_step_df.stack()
+# превращаем в выходной датафрейм
+out_df = stack_df.to_frame().reset_index()
+# Переименовываем колонки
+out_df.rename(columns={'level_0':'Наименование ПОО','level_1':'Место проведения',0:'Количество школьников посетивших профробы'},inplace=True)
+
+out_df.to_excel('Базовый отчет по профориентации.xlsx',index=False)
+#
+# # df.to_excel('temp.xlsx',index=False)
 
